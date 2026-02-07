e061b3d8b69c3beb1c01cc6651c20b217237f395
Chermi6267 <chermi6267@gmail.com>
2026-02-08 02:11:37 +0300

    Added proxy mode switcher

--- src/modules/ProxyMode/index.ts
+++ src/modules/ProxyMode/index.ts
@@ -1,1 +1,2 @@
 export { useProxyModeStore } from "./store/proxyMode.store";
+export { type ProxyMode } from "./types/proxyMode";
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,13 @@
 import "./style.css";
 import DomainList from "modules/DomainList";
 import Label from "widgets/Label/Label";
-import { NODE_ENV } from "env";
+import ProxyModeSwitcher from "modules/ProxyModeSwitcher";
 
 function Main() {
   return (
     <main className="main">
       <Label />
+      <ProxyModeSwitcher />
       <DomainList />
     </main>
   );
